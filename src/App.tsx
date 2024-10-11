b290b73ea7b514c96ee76356867a20d2f086efea
ShubhamMhatre2727 <shubhammhatre2727@gmail.com>
2024-10-11 18:23:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {motion}  from "framer-motion"
+import './App.css'
+import Wheel from './components/wheel'
+
+function App() {
+
+  return (
+      <div className='w-8/12 aspect-square bg-orange-300 -z-50'>
+
+        
+
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Wheel.tsx
@@ -0,0 +1,9 @@
+import {motion} from "framer-motion"
+
+export default function Wheel() {
+  return (
+    <div className="">
+        
+    </div>
+  )
+}
